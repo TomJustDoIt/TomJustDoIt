0d10ee6e683826b7cd0e07f2c98f6720294e0f0e
TomJustDoIt <70694524+TomJustDoIt@users.noreply.github.com>
2022-07-14 15:22:39 +0800

    Update rtx_thread_manager.c

--- rtx_thread_manager.c
+++ rtx_thread_manager.c
@@ -48,3 +48,137 @@ int main(void)
 
   }
 }
+
+
+
+ void Thread_EventSender (
+  void *argument); 
+  // thread function 1
+ 
+
+  void Thread_EventReceiver (
+  void *argument); 
+  // thread function 2
+ 
+
+  osThreadId_t tid_Thread_EventSender; 
+  // thread id 1
+ 
+
+  osThreadId_t tid_Thread_EventReceiver; 
+  // thread id 2
+ 
+ 
+ 
+
+  osEventFlagsId_t evt_id; 
+  // message queue id
+ 
+ 
+ 
+
+  #define FLAGS_MSK1 0x00000001ul
+ 
+
+   
+ 
+
+  void app_main (
+  void)
+ 
+
+  {
+ 
+
+   tid_Thread_EventSender = 
+  osThreadNew (Thread_EventSender, NULL, NULL);
+ 
+ 
+  if (tid_Thread_EventSender == NULL) {
+ 
+
+   ; 
+  // do something
+ 
+
+   }
+ 
+
+   tid_Thread_EventReceiver = 
+  osThreadNew (Thread_EventReceiver, NULL, NULL);
+ 
+ 
+  if (tid_Thread_EventReceiver == NULL) {
+ 
+
+   ; 
+  // do something
+ 
+
+   }
+ 
+
+   ; 
+  // do something
+ 
+
+  }
+ 
+ 
+ 
+
+  void Thread_EventSender (
+  void *argument)
+ 
+
+  {
+ 
+
+   evt_id = 
+  osEventFlagsNew(NULL);
+ 
+ 
+  while (1) { 
+ 
+ 
+  osEventFlagsSet(evt_id, FLAGS_MSK1);
+ 
+ 
+  osDelay (1000); 
+  // suspend thread
+ 
+
+   }
+ 
+
+  }
+ 
+ 
+ 
+
+  void Thread_EventReceiver (
+  void *argument)
+ 
+
+  {
+ 
+
+   uint32_t flags;
+ 
+ 
+  while (1) {
+ 
+
+   flags = 
+  osEventFlagsWait (evt_id,FLAGS_MSK1,
+  osFlagsWaitAny, 
+  osWaitForever);
+ 
+ 
+  //handle event
+ 
+
+   }
+ 
+
+  }
